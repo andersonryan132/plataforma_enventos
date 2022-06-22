1d27158c8240d279fc3bcc1c25f897cd52f6ef8c
Anderson Ryan <51727054+andersonryan132@users.noreply.github.com>
2022-06-21 22:56:48 -0300

    primeiro
    intregração de apis

---
+++ src/lib/apollo.ts
@@ -0,0 +1,6 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+export const client = new ApolloClient({
+    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4otzkn614ts01xkgxxeciyn/master',
+    cache: new InMemoryCache()
+})
